44303eed089a55d15a33d6ed99ba48514583ba2d
Jhon <jhonomeara09@gmail.com>
2024-08-23 08:34:25 -0500

    Completo

--- ProyectoGit/src/proyectogit/Persona.java
+++ ProyectoGit/src/proyectogit/Persona.java
@@ -9,5 +9,39 @@ package proyectogit;
  * @author estudiantes
  */
 public class Persona {
+    private String ced,nom,ape;
+
+    public String getCed() {
+        return ced;
+    }
+
+    public void setCed(String ced) {
+        this.ced = ced;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getApe() {
+        return ape;
+    }
+
+    public void setApe(String ape) {
+        this.ape = ape;
+    }
+
+    public Persona() {
+    }
+
+    public Persona(String ced, String nom, String ape) {
+        this.ced = ced;
+        this.nom = nom;
+        this.ape = ape;
+    }
     
 }
